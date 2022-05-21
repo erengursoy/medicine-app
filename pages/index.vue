3b571fb1cfe16931e82be8f56963be71d8791bf0
erengursoy <eren.centilmen@gmail.com>
2022-05-21 03:18:33 +0300

    first-commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<Users></Users>
+
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            imgList: [
+        "https://picsum.photos/1000/500?image=0",
+        "https://picsum.photos/1000/500?image=1",
+        "https://picsum.photos/1000/500?image=2",
+        "https://picsum.photos/1000/500?image=3",
+        "https://picsum.photos/1000/500?image=4",
+        
+        
+      ],
+        }
+    }
+};
+</script>
+<style lang='scss'>
+
+</style>
